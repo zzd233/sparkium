29c82221f1c67de64f4ae8c80b59cc3c84867d6b
oyyp <oyyp21@mails.tsinghua.edu.cn>
2023-12-18 17:26:57 +0800

    partial subsurface scattering

--- src/sparks/assets/material.h
+++ src/sparks/assets/material.h
@@ -13,6 +13,12 @@ enum MaterialType : int {
   MATERIAL_TYPE_EMISSION = 4
 };
 
+enum MediumType : int {
+  MEDIUM_TYPE_VACUUM = 0,
+  MEDIUM_TYPE_NONSCATTERING = 1,
+  MEDIUM_TYPE_ISOTROPICSCATTERING = 2
+};
+
 class Scene;
 
 struct Material {
@@ -20,9 +26,12 @@ struct Material {
   int albedo_texture_id{0};
   glm::vec3 emission{0.0f};
   float emission_strength{1.0f};
+  // glm::vec3 coef_scattering;
   float refraction_ratio{1.5f};
+  // glm::vec3 coef_absorption;
   float alpha{1.0f};
   MaterialType material_type{MATERIAL_TYPE_LAMBERTIAN};
+  // MediumType medium_type{MEDIUM_TYPE_VACUUM};
   float reserve[1]{};
   Material() = default;
   explicit Material(const glm::vec3 &albedo);
--- src/sparks/shaders/material.glsl
+++ src/sparks/shaders/material.glsl
@@ -4,10 +4,17 @@ struct Material {
   int albedo_texture_id;
   vec3 emission;
   float emission_strength;
+  // vec3 coef_scattering;
   float refraction_ratio;
+  // vec3 coef_absorption;
   float alpha;
   uint material_type;
+  // uint medium_type;
 };
+
+#define MEDIUM_TYPE_VACUUM 0
+#define MEDIUM_TYPE_NONSCATTERING 1
+#define MEDIUM_TYPE_ISOTROPICSCATTERING 2
 
 #define MATERIAL_TYPE_LAMBERTIAN 0
 #define MATERIAL_TYPE_SPECULAR 1
